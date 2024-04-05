c2e5b94a84c9a92625c27b069c772660fe5e3afa
anmoljain16 <ajanmoldosi@gmail.com>
2024-04-05 22:38:10 +0530

    Trying fixing json a token error

--- components/Home/hero.jsx
+++ components/Home/hero.jsx
@@ -4,7 +4,9 @@ export default function Hero(){
             <div className="px-12 mx-auto max-w-7xl">
                 <div className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
                     <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
-                        <span>Start</span> <span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-green-400 to-purple-500 lg:inline">building a buzz</span> <span>around your product?</span>
+                        <span>Start</span> <span
+                        className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-green-400 to-purple-500 lg:inline">building a buzz</span>
+                        <span>around your product?</span>
                     </h1>
                     <p className="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">
                         Start gaining the traction you've always wanted with our next-level templates and designs. Crafted to help you tell your story.
